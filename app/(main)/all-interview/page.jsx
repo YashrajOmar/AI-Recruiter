cc44a7835723aeac645e666978a9ad86c04832cb
Yashraj Omar <2023eeb1258@iitrpr.ac.in>
2025-06-07 00:59:26 +0530

    yashraj 6

---
+++ app/(main)/all-interview/page.jsx
@@ -0,0 +1,56 @@
+"use client"
+import { Button } from '@/components/ui/button';
+import { Camera, Video } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { supabase } from '@/services/supabaseClient';
+import { Inter } from 'next/font/google';
+import { useUser } from '@/app/provider';
+import { toast } from 'sonner';
+import InterviewCard from '../dashboard/_components/interviewCard';
+
+
+function AllInterview() {
+    const [interviewList,setInterviewList]=useState([]);
+  const {user}=useUser();
+
+  useEffect(() => {
+    user && GetInterviewList();
+  }, [user]);
+
+
+  const GetInterviewList= async()=>{
+    let { data: Interviews, error } = await supabase
+      .from('Interviews')
+      .select('*')
+      .eq('userEmail', user?.email)
+      .order('id', { ascending: false })
+
+    console.log(Interviews);
+    setInterviewList(Interviews);
+  }
+
+  
+    return (
+    
+    <div className='my-5'><h2 className='font-bold text-2xl'>
+        All Previously Created Interviews </h2>
+
+        
+        {interviewList?.length==0&&
+        <div className='p-5 flex flex-col gap-3 items-center mt-5 bg-white rounded-lg'>
+            <Video className='h-10 w-10 text-primary'/>
+            <h2>You don't have any interview created!</h2>
+            <Button>+ Create New Interview</Button>
+
+            </div>}
+            {interviewList&&
+               <div className='grid grid-cols-2 mt-5 xl:grid-cols-4 gap-5'>
+                {interviewList.map((interview, index) => (
+                    <InterviewCard interview={interview} key={index} />
+                ))}
+               </div>
+            }
+        </div>
+  )
+}
+export default AllInterview;
--- app/(main)/dashboard/_components/interviewCard.jsx
+++ app/(main)/dashboard/_components/interviewCard.jsx
@@ -3,12 +3,16 @@ import moment from 'moment';
 import React from 'react';
 import { toast } from 'sonner';
 
-function InterviewCard({ interview }) {
+function InterviewCard({ interview, viewDetail=false }) {
 
   const copyLink=() => {
     const url = process.env.NEXT_PUBLIC_HOST_URL+'/'+interview?.interview_id;
     navigator.clipboard.writeText(url);
     toast('Copied')
+  }
+
+  const onSend=()=>{
+      window.location.href="mailto:accounts@tubeguruji.com?subject=AI Recruiter Interview Link&body=Hi, I would like to share the interview link with you: "+process.env.NEXT_PUBLIC_HOST_URL+'/'+interview?.interview_id;
   }
 
   return (
@@ -19,11 +23,17 @@ function InterviewCard({ interview }) {
 
         </div>
         <h2 className='mt-3 font-bold text-lg'>{interview?.jobPosition}</h2>
-        <h2 className='mt-2'>{interview?.duration}</h2>
-        <div className='flex gap-3 w-full mt-5'>
+        <h2 className='mt-2'>{interview?.duration}
+          <span>{interview?.interview-feedback
+      }</span>
+        </h2>
+        {!viewDetail? <div className='flex gap-3 w-full mt-5'>
           <Button variant='outline' className={'w-full'} onClick={copyLink} > <Copy /> Copy Link</Button>
-          <Button className={'w-full'}> <Send /> Send</Button>
+          <Button className={'w-full'} onClick={onSend}> <Send /> Send</Button>
         </div>
+        :
+        <Button className="mt-5 w-full" variant="outline">View Detail <ArrowRight/></Button>
+        }
     </div>
   );
 }
